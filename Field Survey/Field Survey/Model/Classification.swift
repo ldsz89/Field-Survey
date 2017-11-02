4c391dad4fd94337960f666baae6a14b795b2e82
Shannon <ldsz89@mail.missouri.edu>
2017-11-02 18:31:46 -0500

    Added Model files for Classification and Observation

---
+++ Field Survey/Field Survey/Model/Classification.swift
@@ -0,0 +1,23 @@
+//
+//  Classification.swift
+//  Field Survey
+//
+//  Created by Shannon, Leandre D. (MU-Student) on 11/2/17.
+//  Copyright © 2017 Shannon, Leandre D. (MU-Student). All rights reserved.
+//
+
+import UIKit
+
+enum Classification: String {
+    case amphibian
+    case bird
+    case fish
+    case insect
+    case mammal
+    case plant
+    case reptile
+    
+    var image: UIImage? {
+        return UIImage(named: self.rawValue + "Icon")
+    }
+}
---
+++ Field Survey/Field Survey/Model/Observation.swift
@@ -0,0 +1,31 @@
+//
+//  Observation.swift
+//  Field Survey
+//
+//  Created by Shannon, Leandre D. (MU-Student) on 11/2/17.
+//  Copyright © 2017 Shannon, Leandre D. (MU-Student). All rights reserved.
+//
+
+import Foundation
+
+struct Observation {
+    let classification: Classification
+    let title: String
+    let description: String
+    let date: Date
+    
+    init(classification: Classification, title: String, description: String, date: Date) {
+        self.classification = classification
+        self.title = title
+        self.description = description
+        self.date = date
+    }
+    
+    init?(classificationName: String, title: String, description: String, date: Date) {
+        if let classification = Classification(rawValue: classificationName) {
+            self.init(classification: classification, title: title, description: description, date: date)
+        } else {
+            return nil
+        }
+    }
+}
