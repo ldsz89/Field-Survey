77d310e21df3af67a712888f9772e387c137b99b
Shannon <ldsz89@mail.missouri.edu>
2017-11-02 17:32:04 -0500

    Created Project

---
+++ Field Survey/Field Survey/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Field Survey
+//
+//  Created by Shannon, Leandre D. (MU-Student) on 11/2/17.
+//  Copyright © 2017 Shannon, Leandre D. (MU-Student). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
