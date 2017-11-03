8d28c250eb40d4288971981c744a47cc322f4bbe
Shannon <ldsz89@mail.missouri.edu>
2017-11-02 20:51:35 -0500

    Working version of app

--- Field Survey/Field Survey/ObservationsViewController.swift
+++ Field Survey/Field Survey/ObservationsViewController.swift
@@ -9,9 +9,17 @@
 import UIKit
 
 class ObservationsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    @IBOutlet weak var observationsTableView: UITableView!
+    
+    let observations = ObservationsJSONLoader.load(fileName: "field_observations")
+    
+    let dateFormatter = DateFormatter()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        dateFormatter.dateStyle = .medium
+        dateFormatter.timeStyle = .medium
 
         // Do any additional setup after loading the view.
     }
@@ -19,6 +27,34 @@ class ObservationsViewController: UIViewController, UITableViewDelegate, UITable
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return observations.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "fieldObservationCell", for: indexPath)
+        
+        if let cell = cell as? ObservationTableViewCell {
+            let observation = observations[indexPath.row]
+            cell.observationIconImageView.image = observation.classification.image
+            cell.titleLabel.text = observation.title
+            cell.dateLabel.text = dateFormatter.string(from: observation.date)
+        }
+        
+        return cell
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let destination = segue.destination as? ObservationDetailViewController,
+            let row = observationsTableView.indexPathForSelectedRow?.row {
+            destination.observation = observations[row]
+        }
     }
     
 
